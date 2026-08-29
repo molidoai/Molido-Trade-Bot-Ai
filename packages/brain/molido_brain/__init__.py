f1e81140fdc2827410a473d907dbfa18c059aae8
molidoai <molidoai@gmail.com>
2026-08-29 19:57:32 +0330

    Copy brain/shared/portfolio package inits from tighten-risk

--- packages/brain/molido_brain/__init__.py
+++ packages/brain/molido_brain/__init__.py
@@ -1,3 +1,27 @@
 from molido_brain.engine import DecisionBrain, BrainDecision
+from molido_brain.features import extract_features, h1_side_from_bars
+from molido_brain.universe import (
+    DEFAULT_UNIVERSE,
+    UniversePicker,
+    CheapCandidate,
+    resolve_universe,
+    resolve_trade_timeframe,
+    cheap_score,
+    is_auto_symbols,
+    is_auto_timeframe,
+)
 
-__all__ = ["DecisionBrain", "BrainDecision"]
+__all__ = [
+    "DecisionBrain",
+    "BrainDecision",
+    "extract_features",
+    "h1_side_from_bars",
+    "DEFAULT_UNIVERSE",
+    "UniversePicker",
+    "CheapCandidate",
+    "resolve_universe",
+    "resolve_trade_timeframe",
+    "cheap_score",
+    "is_auto_symbols",
+    "is_auto_timeframe",
+]
